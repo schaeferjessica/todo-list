bd20a8d67d4898d6b17098ad687033005566bdad
jschaefer <jschaefer@3pc.de>
2020-11-15 20:29:08 +0100

    feat: add local storage function

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ const todoList = document.querySelector('.todo__list');
 const todoFilter = document.querySelector('.todo__filter');
 
 // event listener
+document.addEventListener('DOMContentLoaded', () => getLocalStorage());
 todoBtn.addEventListener('click', event => {
-  if(todo.value !== "") {
-    addTodo(event);
-  } else {
-    event.preventDefault();
+  event.preventDefault();
+
+  const todoValue = todo.value;
+  if(todoValue !== "") {
+    addTodo(todoValue);
+    saveLocalStorage(todoValue);
   }
 });
 
 todoFilter.addEventListener('click', event => filterTodo(event));
 
 // function 
-const addTodo = event => {
-  event.preventDefault();
-  const todoLi = `<li class="todo__item"><button class="todo__delete">-</button><button class="todo__check">✓</button><span>${todo.value}</span></li>`
+const addTodo = todoValue => {
+  const todoLi = `<li class="todo__item"><button class="todo__delete">-</button><button class="todo__check">✓</button><span>${todoValue}</span></li>`
   todoList.insertAdjacentHTML('beforeend', todoLi);
   todo.value = "";
   const todoItems = document.querySelectorAll('.todo__item');
@@ -26,11 +28,14 @@ const addTodo = event => {
   const todoBtnCheck = lastItem.querySelector('.todo__check');
   const todoBtnDelete = lastItem.querySelector('.todo__delete');
   todoBtnCheck.addEventListener('click', () => checkItem(lastItem));
-  todoBtnDelete.addEventListener('click', () => deleteItem(lastItem));
+  todoBtnDelete.addEventListener('click', () => deleteItem(lastItem, todoValue));
 }
 
 const checkItem = lastItem => lastItem.classList.toggle("done");
-const deleteItem = lastItem => lastItem.remove();
+const deleteItem = (lastItem, todoValue)=> {
+  lastItem.remove()
+  removeLocalStorage(todoValue)
+};
 
 const filterTodo = event => {
   const todos = todoList.childNodes;
@@ -57,6 +62,48 @@ const filterTodo = event => {
   });
 }
 
+const saveLocalStorage = inputValue => {
+  let inputValues;
+  if (localStorage.getItem("inputValues") === null) {
+    inputValues = [];
+  } else {
+    inputValues = JSON.parse(localStorage.getItem("inputValues"));
+  }
+
+  inputValues.push(inputValue);
+  localStorage.setItem("inputValues", JSON.stringify(inputValues));
+}
+
+const getLocalStorage = () => { 
+  let inputValues;
+  if (localStorage.getItem("inputValues") === null) {
+    inputValues = [];
+    return;
+  } else {
+    inputValues = JSON.parse(localStorage.getItem("inputValues"));
+  }
+
+  inputValues.forEach(inputValue => addTodo(inputValue));
+}
+
+const removeLocalStorage = inputValue => {
+  let inputValues;
+  if (localStorage.getItem("inputValues") === null) {
+    inputValues = [];
+    return;
+  } else {
+    inputValues = JSON.parse(localStorage.getItem("inputValues"));
+  }
+  inputValues.splice(inputValues.indexOf(inputValue), 1);
+  localStorage.setItem("inputValues", JSON.stringify(inputValues));
+}
 
 //$('.klasse', body)
 //const $ = (myEl, where) => where.querySelector(myEl);
+
+// const value = 'jessi';
+// addAgeByOne(value);
+
+// const addAgeByOne = inputValue => {
+//   console.log(inputValue);
+// }
